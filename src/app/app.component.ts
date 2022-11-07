b858265993593750c11ccc82a0a0a5e5ab2e592a
KotKamil <62114754+KotKamil@users.noreply.github.com>
2022-11-07 08:27:10 +0100

    Kalkulator w 40minut w Angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'kalkulator';
+	title = 'kalkulator';
+	panelValue = "";
+	wstaw = (numberValue: string) => {
+		this.panelValue += numberValue;
+	}
+
+	oblicz = () => {
+		this.panelValue = eval(this.panelValue);
+	}
+
+	wyczysc = () => {
+		this.panelValue = "";
+	}
 }
